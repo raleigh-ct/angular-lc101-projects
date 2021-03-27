e8a53c90548ef7abee1621fa5082de5670e5ec0d
Raleigh Moore <raleighcmoore@gmail.com>
2021-03-27 15:50:41 -0500

    updated components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Amazing Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.cleveland19.com/resizer/oYdtj4IDxWdSSKn7GoDAHjs2o8U=/1200x600/arc-anglerfish-arc2-prod-raycom.s3.amazonaws.com/public/GIDLHC3O2ZDWNLJD57OMCIBBUM.jpg';
+  image3 = 'https://www.cleveland.com/resizer/T8ecyXXN8GKeBZj5EBaFZZYsEGk=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/JWT6QGXJQ5BXHKACU4WLWSV54M.jpg';
 
   constructor() { }
 
